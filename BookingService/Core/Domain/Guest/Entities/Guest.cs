21bc1bc9059102a85c9d3c2edafa1a16d69933ff
Donathan <donathanramalho@yahoo.com.br>
2024-09-03 22:02:18 -0300

    Criação de entidades / Máquina de estados

---
+++ BookingService/Core/Domain/Guest/Entities/Guest.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int Surname { get; set; }
+        public int Email { get; set; }
+    }
+}
---
+++ BookingService/Core/Domain/Room/Entities/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int InMaintenance { get; set; }
+    }
+}
